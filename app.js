b2e92b320f74f1c1c73aaed1af6e2a6ff990dc27
NK <kiteretu97@gmail.com>
2017-03-01 14:30:14 +0900

    3

---
+++ app.js
@@ -0,0 +1,19 @@
+	const http = require('http');
+	const fs = require('fs');
+	const socketio = require('socket.io');
+	
+	const port = 3000;
+	
+	const server = http.createServer(function(req,res){
+		res.writeHead(200,{'Content-Type' : 'text'});
+		res.end(fs.readFileSync(__dirname + '/client/index.html'),'utf8');
+		}).listen(port);
+	
+	const io = socketio.listen(server);
+	io.sockets.on('connection',function(socket){
+		socket.on('message',function(data){
+		console.log(data.value);
+			io.sockets.emit('from_server',{value:data.value});
+		 	});
+		});
+	
